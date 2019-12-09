5d02991bfd16f1e89bcb0b547072a8f7a5e7b766
sokphorn.teng <sokphorn.teng@PNC.LAN>
2019-12-09 20:23:04 +0700

    take name from API

--- js/ajax.js
+++ js/ajax.js
@@ -1,51 +1,23 @@
-$(document).ready( () =>{
-  $('#recipe').on('change', ()=>{
-    var fruit = $('#recipe').val();
-    choose(fruit);
-    requestAPI();
+$(document).ready(function(){
+  takeAPI();
+  $('#sert').on('change', () =>{
+    var sert = $('#sert').val();
+    getRecipe(sert);
+  })
+});
+function takeAPI() {
+  var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+$.ajax({
+    dataType:'JSON',
+    url: url,
+    success: (data) =>chooseRecipe(data.recipes),
+    error:() => console.log("Can not get data"),
 })
-})
-     var choose = (data)=>{
-       switch(parseInt(data)) {
-         case 1:
-         getNomPom();
-         break;
-         case 2:
-         getAvocado();
-         break;
-        }
-      }
-      // get apple
-      var getNomPom = () =>{
-        var apple = "Nom Pom";
-        printOut(apple);
-      }
-      var getAvocado = () =>{
-        var coconut = "Avocado Shake (Toek Kalok)";
-        printOut(coconut);
-      }
-      var printOut = (out) =>{
-        $('#done').html(out);
-      }
-
-      var requestAPI = () =>{
-        var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-          $.ajax({
-            url: url,
-            dataType:'JSON',
-            success: function(data){
-              var rest ="";
-              data.recipes.forEach(item =>{
-                if(item.id == 1){
-                rest +=`
-                   <div class="card">
-                    <img src="${item.iconUrl}">
-                   </div>
-                `;
-                }
-              })
-              $('#done').append(rest);
-            }
-          })
-   } 
-
+}
+function chooseRecipe(done){
+var option = "";
+done.forEach(item =>{
+    option +=`<option value="${item.id}">${item.name}</option>`;
+});
+$('#sert').append(option);
+}
