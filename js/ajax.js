36d2c9938e0322fb8c315be71dc86c42c45690a9
sokphorn.teng <sokphorn.teng@pnc.lan>
2019-12-11 20:39:52 +0700

    take the recipe into table

--- js/ajax.js
+++ js/ajax.js
@@ -1,23 +1,68 @@
-$(document).ready(function(){
-  takeAPI();
-  $('#sert').on('change', () =>{
-    var sert = $('#sert').val();
-    getRecipe(sert);
-  })
-});
-function takeAPI() {
+function getUrl() {
   var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-$.ajax({
-    dataType:'JSON',
-    url: url,
-    success: (data) =>chooseRecipe(data.recipes),
-    error:() => console.log("Can not get data"),
-})
+  return url;
 }
-function chooseRecipe(done){
-var option = "";
-done.forEach(item =>{
-    option +=`<option value="${item.id}">${item.name}</option>`;
+$(document).ready(function() {
+  getApi();
+  $('#sert').on('change',function(){
+      var recipeId = $('#sert').val();
+     eachValue(recipeId);
+  });
 });
-$('#sert').append(option);
+function getApi() {
+  $.ajax({
+      dataType: 'json',
+      url: getUrl(),
+      success:(data) => takeAll(data.recipes),
+      error: () => console.log("Can not use data"),
+      
+  });
 }
+var allData = [];
+function takeAll(recipe) {
+  allData = recipe;
+  var option = "";
+  recipe.forEach(item => {
+     option += `<option value = "${item.id}">${item.name}</option>`;
+  });
+  $('#sert').append(option);
+}
+function eachValue(id){
+  allData.forEach(item => {
+      if(item.id == id){
+       showRecipe(item.name, item.iconUrl);  
+       getSelection(item.ingredients);
+      }
+  });
+}
+function showRecipe(name, img) {
+  var result = "";
+  result += `<img src= "${img}" width ="90%">
+  <h2>${name}</h2>
+  `;
+  $('#recipe-result').html(result);
+}
+function getSelection (add){
+  var store = "";
+  add.forEach(item => {
+     store +=`
+      <tr>
+          <td><img src="${item.iconUrl}" width = "80" ></td>
+          <td>${item.name}</td>
+          <td>${item.quantity}</td>
+          <td>${item.unit}</td>
+      </tr>
+     `;
+  });
+  $('#show').html(store);
+}
+
+
+
+
+
+
+
+
+
+
