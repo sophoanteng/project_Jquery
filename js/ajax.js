ac72048a3250514cbe238c56d26158dfb0fccb96
sokphorn.teng <sokphorn.teng@pnc.lan>
2019-12-12 20:54:24 +0700

    get step

--- js/ajax.js
+++ js/ajax.js
@@ -32,13 +32,19 @@ function eachValue(id){
       if(item.id == id){
        showRecipe(item.name, item.iconUrl);  
        getSelection(item.ingredients);
+       getStep(item.instructions);
       }
   });
 }
 function showRecipe(name, img) {
   var result = "";
-  result += `<img src= "${img}" width ="90%">
-  <h2>${name}</h2>
+  result += `
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3"><h2>${name}</h2></div>
+        <div class="col-3"><img src= "${img}" width ="100%"></div>
+        <div class="col-3"></div>
+      </div>
   `;
   $('#recipe-result').html(result);
 }
@@ -47,14 +53,26 @@ function getSelection (add){
   add.forEach(item => {
      store +=`
       <tr>
-          <td><img src="${item.iconUrl}" width = "80" ></td>
+          <td><img src="${item.iconUrl}" width = "90px" ></td>
           <td>${item.name}</td>
           <td>${item.quantity}</td>
           <td>${item.unit}</td>
       </tr>
      `;
   });
-  $('#show').html(store);
+  $('#ingredient').html(store);
+}
+function getStep(step){
+let cutStep = step.split('<step>');
+  var resultStep ="";
+   for(let i=1; i<cutStep.length; i++){
+     resultStep +=`
+     Step ${i} :
+       ${cutStep[i]}
+       <br>
+     `;
+   }
+  $('#step').html(resultStep);
 }
 
 
